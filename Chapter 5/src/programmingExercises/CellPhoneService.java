ebd63f57b3b858d45833738c4cb51481a09736c9
Vaughn01 <tv111790@students.davenport.k12.ia.us>
2017-02-21 14:59:01 -0600

    Again 2/21/17

--- Chapter 5/src/programmingExercises/CellPhoneService.java
+++ Chapter 5/src/programmingExercises/CellPhoneService.java
@@ -3,6 +3,8 @@
  */
 package programmingExercises;
 
+import javax.swing.JOptionPane;
+
 /**
  * @author TV111790
  *
@@ -14,6 +16,28 @@ public class CellPhoneService {
 	 */
 	public static void main(String[] args) 
 	{
+		
+		double planA = 49.00;
+		
+		double planB = 55.00;
+		
+		double planC = 61.00;
+		
+		double planD = 70.00;
+		
+		double planE;
+		
+		double planF;
+		
+		double userMin;
+		
+		double userText;
+		
+		double userData;
+		
+		int msg;
+		
+		
 		
 		
 
