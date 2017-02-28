78f52408265b66f643fc8f50dc1b9adb949a9fbc
Vaughn01 <tv111790@students.davenport.k12.ia.us>
2017-02-28 13:51:16 -0600

    2/28/17

---
+++ Chapter 6/src/youDoIt/DivideEvenly.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package youDoIt;
+
+/**
+ * @author TV111790
+ *
+ */
+public class DivideEvenly {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		
+		final int LIMIT = 100;
+		int var;
+		
+		System.out.print(LIMIT + "is evenly divisible by ");
+		
+		for(var = 1; var <= LIMIT; ++var)
+			if(LIMIT % var == 0)
+				System.out.print(var + " ");
+		System.out.println();
+		
+
+	}
+
+}
